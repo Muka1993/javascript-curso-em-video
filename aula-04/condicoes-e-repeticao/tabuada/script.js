641b985319680bd3bb634abcf5fdae0b4840eef6
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2023-02-03 16:17:16 -0300

    Atualizando

---
+++ aula-04/condicoes-e-repeticao/tabuada/script.js
@@ -0,0 +1,8 @@
+
+function gerarTabuada() {
+    let num = document.getElementById('txtn');
+    let tab = document.getElementById('seltab')
+    if(num.ariaValueMax.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }
+}
